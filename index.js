3145e2f02f4d456fdae850de44def765bbd0a5a7
Diogo Felicio Demétrio Pereira <pereira.diogo741@gmail.com>
2024-05-26 16:57:26 -0300

    alteracoes basicas

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+
 const nedb = require('nedb');
 const dataBase = new nedb('database.db');
 dataBase.loadDatabase();
